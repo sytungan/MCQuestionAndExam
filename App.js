352b70922ddbbc8e34f5c1215ba2945e2841712e
tuananh0209 <huynhtuananh0209@gmail.com>
2020-12-29 15:52:30 +0700

    add

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { ThemeProvider } from 'react-native-elements';
 import RootNavigator from './src/navigation/RootNavigator';
-import Login from './src/components/login/Login'
+import Login from './src/components/login/Login';
+import Contribute from './src/components/contributeQuestion/Contribute'
 import { View , Text } from 'react-native';
+
+
 export default class App extends Component {
     render() {
     return (
         <ThemeProvider useDark={false}>
         {/* <RootNavigator />  */}
-         <Login/> 
+         <Contribute/>
         
          </ThemeProvider> 
        
